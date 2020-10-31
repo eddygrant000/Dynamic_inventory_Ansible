a7fcbd93c04dbbfeeabe549bc3096ec94ed1877e
Sachin Saini <45393031+eddygrant000@users.noreply.github.com>
2020-10-31 09:51:48 +0530

    Create dynamic_inventory.py

---
+++ dynamic_inventory.py
@@ -0,0 +1,25 @@
+#!/bin/python3.7
+
+import boto3
+import os
+
+ec2 = boto3.resource('ec2')
+html = """<html><table border="1">
+<tr><th>Instance-Name</th><th>Instance-ID</th><th>launch-time</th><th>State</th><th>RootDevice</th><th>Private-IP-Address</th><th>Public-IP-Address</th><th>Subnet</th></tr>"""
+for instance in ec2.instances.all():
+    names = [tag.get('Value') for tag in instance.tags if tag.get('Key') == 'Name']
+    hname = names[0] if names else None
+    html += "<tr><td>{}</td>".format(hname)
+    html += "<td>{}</td>".format(instance.id)
+    html += "<td>{}</td>".format(instance.launch_time)
+    html += "<td>{}</td>".format(instance.state['Name'])
+    html += "<td>{}</td>".format(instance.root_device_name)
+    html += "<td>{}</td>".format(instance.private_ip_address)
+    html += "<td>{}</td>".format(instance.public_ip_address)
+    html += "<td>{}</td>".format(instance.subnet)
+    html += "</tr>"
+html += "</table></html>"
+file_ = open('result.html', 'w')
+file_.write(html)
+file_.close()
+os.system('firefox result.html')
